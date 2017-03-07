b794abe804cbe128ec6586d6c04265bd98c7ce7d
sbhole <sandip.bhole@nice.com>
2017-03-07 17:05:24 +0530

    SuiteRun.bat

---
+++ runSuite.bat
@@ -0,0 +1,12 @@
+set CLASSPATH=./lib/extJars;./lib/pythonlib;./lib/robotlib;./;
+set PYTHONPATH=./lib/extJars;./lib/pythonlib;./lib/robotlib;./;
+set RESOURCES=./resources;
+
+echo %PYTHONPATH%
+echo %CLASSPATH%
+echo %RESOURCES%
+
+
+C:\Python27\Scripts\robot.bat  ./testsuites/
+
+ 
